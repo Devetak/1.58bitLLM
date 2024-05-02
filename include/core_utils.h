122371c123d983b43c010c133a26d7048d5f6217
Mitja Devetak <m888itja@gmail.com>
2024-05-02 20:18:07 +0200

    matrix vector multiplication

--- include/core_utils.h
+++ include/core_utils.h
@@ -5,4 +5,6 @@
 // where a is a 32 bit integer which holds 16 2-bit values
 // where b is a 16 elements 8 bit array
 // where r is a single 8 bit array for the output
-void inner_product_bitwise(int8_t* r, int32_t* a, int8_t* b);
+void inner_product_bitwise_inplace(int8_t* r, int32_t a, int8_t* b);
+int8_t inner_product_bitwise(int32_t a, int8_t* b);
+void matrix_vector_bitwise(int8_t* r, int32_t* a, int8_t* W, int heigth, int width);
--- src/core_utils.cpp
+++ src/core_utils.cpp
@@ -1,18 +1,55 @@
 #include "core_utils.h"
 #include <stdint.h>
 #include <iostream>
+#include <stdio.h>
 
-void inner_product_bitwise(int8_t* r, int32_t* a, int8_t* b) {
+void printBits(int num) {
+    for (int i = 31; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        printf("%d", bit);
+    }
+    printf("\n");
+}
+
+void inner_product_bitwise_inplace(int8_t* r, int32_t a, int8_t* b) {
 
     *r = 0;
     int prod;
     for(int i = 0; i < 16; ++i){
-        prod = (*a & 3) == 0 ? 0 : ((*a & 3) == 1 ? 1 : -1);
+        prod = (a & 3) == 0 ? 0 : ((a & 3) == 1 ? 1 : -1);
         *r += b[15 - i] * prod;
-        *a = *a >> 2;
+        a = a >> 2;
     }
 
     return;
+}
+
+int8_t inner_product_bitwise(int32_t a, int8_t* b){
+    int8_t r = 0;
+    int prod;
+    for(int i = 0; i < 16; ++i){
+        prod = (a & 3) == 0 ? 0 : ((a & 3) == 1 ? 1 : -1);
+        r += b[15 - i] * prod;
+        a = a >> 2;
+    }
+
+    return r;
+}
 
 
+void matrix_vector_bitwise(int8_t* r, int32_t* W, int8_t* x, int heigth, int width){
+
+    int entries = width / 16;
+
+    for(int i = 0; i < heigth; ++i){
+        r[i] = 0;
+    }
+
+    for(int i = 0; i < heigth; ++i){
+        for(int j = 0; j < entries; ++j){
+            r[i] += inner_product_bitwise(W[j + i * entries], &x[j * 16]);
+        }
+    }
+
+    return;
 }
